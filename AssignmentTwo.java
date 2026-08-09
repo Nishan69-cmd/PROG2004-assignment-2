0c02897e180bce7540c85beb2afed414601606a8
Nishan69-cmd <nishanbanskota9@gmail.com>
2026-08-09 17:44:53 +0800

    completed and tested staff

---
+++ AssignmentTwo.java
@@ -0,0 +1,23 @@
+public class AssignmentTwo {
+
+    public static void main(String[] args) {
+
+        // Part 1 - Modelling the park's people
+
+        Staff staff1 = new Staff(
+                101,
+                "John Smith",
+                35,
+                "Ride Operator"
+        );
+        Staff staff2 = new Staff(
+        -5,
+        "Emma Brown",
+        29,
+        "Show Operator"
+    );
+
+        System.out.println("STAFF DETAILS");
+        System.out.println(staff1);
+    }
+}
--- Staff.java
+++ Staff.java
@@ -1,15 +1,39 @@
+import java.util.Objects;
+
 public class Staff {
     private int staffID;
     private String name;
     private int age;
     private String role;
 
-    public Staff(int staffID, String name, int age, String role){
-        this.staffID = staffID;
-        this.name = name;
-        this.age = age;
-        this.role = role;
+     public Staff(int staffID, String name, int age, String role) {
+
+    if (staffID <= 0) {
+        throw new IllegalArgumentException("Staff ID must be greater than 0");
     }
+
+    Objects.requireNonNull(name, "Name must not be null");
+
+    if (name.isBlank()) {
+        throw new IllegalArgumentException("Name must not be blank");
+    }
+
+    if (age < 0) {
+        throw new IllegalArgumentException("Age must not be negative");
+    }
+
+    Objects.requireNonNull(role, "Role must not be null");
+
+    if (role.isBlank()) {
+        throw new IllegalArgumentException("Role must not be blank");
+    }
+
+    this.staffID = staffID;
+    this.name = name;
+    this.age = age;
+    this.role = role;
+    }
+   
     public int getStaffID() {
     return staffID;
     }
@@ -32,5 +56,6 @@ public class Staff {
                 + "\nAge: " + age
                 + "\nRole: " + role;
     }
+   
 }
 
