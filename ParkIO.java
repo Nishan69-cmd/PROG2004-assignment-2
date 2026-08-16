e24c37214a84bcb9f44b52ae045a5d90516f5522
Nishan69-cmd <nishanbanskota9@gmail.com>
2026-08-16 17:09:49 +0800

    added visitors queues and histories to park backup

--- ParkIO.java
+++ ParkIO.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 
 public class ParkIO {
 
@@ -13,8 +14,75 @@ public class ParkIO {
             BufferedWriter writer =
                     new BufferedWriter(
                             new FileWriter(fileName));
+s
+            HashSet<Integer> savedStaffIDs =
+                    new HashSet<>();
 
             for (Attraction attraction : park.getAttractions()) {
+
+                Staff operator = attraction.getOperator();
+
+                if (operator != null
+                        && !savedStaffIDs.contains(
+                                operator.getStaffID())) {
+
+                    writer.write(
+                            "STAFF"
+                                    + SEPARATOR
+                                    + operator.getStaffID()
+                                    + SEPARATOR
+                                    + operator.getName()
+                                    + SEPARATOR
+                                    + operator.getAge()
+                                    + SEPARATOR
+                                    + operator.getRole());
+
+                    writer.newLine();
+
+                    savedStaffIDs.add(
+                            operator.getStaffID());
+                }
+            }
+
+          
+            HashSet<Visitor> savedVisitors =
+                    new HashSet<>();
+
+            for (Attraction attraction : park.getAttractions()) {
+
+                savedVisitors.addAll(
+                        attraction.getWaitingVisitors());
+
+                savedVisitors.addAll(
+                        attraction.getVisitHistory());
+            }
+
+            for (Visitor visitor : savedVisitors) {
+
+                writer.write(
+                        "VISITOR"
+                                + SEPARATOR
+                                + visitor.getVisitorID()
+                                + SEPARATOR
+                                + visitor.getName()
+                                + SEPARATOR
+                                + visitor.getAge()
+                                + SEPARATOR
+                                + visitor.getTicketType());
+
+                writer.newLine();
+            }
+
+            for (Attraction attraction : park.getAttractions()) {
+
+                int operatorID = 0;
+
+                if (attraction.getOperator() != null) {
+
+                    operatorID =
+                            attraction.getOperator()
+                                    .getStaffID();
+                }
 
                 if (attraction instanceof Ride) {
 
@@ -25,7 +93,9 @@ public class ParkIO {
                                     + SEPARATOR
                                     + attraction.getName()
                                     + SEPARATOR
-                                    + attraction.getCapacity());
+                                    + attraction.getCapacity()
+                                    + SEPARATOR
+                                    + operatorID);
 
                     writer.newLine();
 
@@ -38,12 +108,44 @@ public class ParkIO {
                                     + SEPARATOR
                                     + attraction.getName()
                                     + SEPARATOR
-                                    + attraction.getCapacity());
+                                    + attraction.getCapacity()
+                                    + SEPARATOR
+                                    + operatorID);
 
                     writer.newLine();
                 }
             }
 
+            for (Attraction attraction : park.getAttractions()) {
+
+                for (Visitor visitor :
+                        attraction.getWaitingVisitors()) {
+
+                    writer.write(
+                            "QUEUE"
+                                    + SEPARATOR
+                                    + attraction.getAttractionID()
+                                    + SEPARATOR
+                                    + visitor.getVisitorID());
+
+                    writer.newLine();
+                }
+            }
+            for (Attraction attraction : park.getAttractions()) {
+
+                for (Visitor visitor :
+                        attraction.getVisitHistory()) {
+
+                        writer.write(
+                                "HISTORY"
+                                        + SEPARATOR
+                                        + attraction.getAttractionID()
+                                        + SEPARATOR
+                                        + visitor.getVisitorID());
+
+                        writer.newLine();
+                }
+                }
             writer.close();
 
             System.out.println(
